561f109d21c4827a775439e105028ea315f854c2
Jason Deng <foralief@gmail.com>
2021-01-23 00:38:31 -0600

    Finished Part 1

---
+++ src/components/Jobs.js
@@ -0,0 +1,43 @@
+import React, {Component} from "react";
+
+import "./Jobs.css";
+import "../App.css";
+
+import JobsListItem from "./JobsListItem";
+
+import axios from "axios";
+
+class Jobs extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			jobs: [],
+		};
+	}
+
+	componentDidMount() {
+		axios.get("/api/jobs").then(({data}) => {
+			this.setState({jobs: data});
+		});
+	}
+
+	render() {
+		const jobsJSX = this.state.jobs.map((job, index) => {
+			return <JobsListItem key={index} {...job} />;
+		});
+
+		return (
+			<div>
+				<div className="App">
+					<header className="App-header">
+						<h1 className="App-title">Jobs In Atlanta</h1>
+						<p className="App-subtitle">Click to explore jobs</p>
+					</header>
+				</div>
+				<div className="Jobs">{jobsJSX}</div>
+			</div>
+		);
+	}
+}
+
+export default Jobs;
